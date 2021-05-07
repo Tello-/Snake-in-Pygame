c6c6666d6d53bae0dfa4321db83a70d155084f8a
Tello- <lollisjosh@gmail.com>
2021-05-07 11:05:11 -0700

    updated color flipping code in splash

--- scene.py
+++ scene.py
@@ -34,14 +34,15 @@ class Splash_Scene(Scene):
     def __init__(self, size:tuple, coord:tuple = (0,0)):
         super().__init__(size, coord)
         self._splash_over = False
-        self.LOGO_CURRENT_COLOR = LOGO_COLOR_1
+        self.LOGO_CURRENT_COLOR = 0 # COLOR 1   
+        self.LOGO_COLORS = [LOGO_COLOR_1, LOGO_COLOR_2, LOGO_COLOR_3, LOGO_COLOR_4, LOGO_COLOR_5]
         self.LOGO_FONT = pygame.font.Font("alba.super.ttf",75 )
         self.LOGO_TEXT = self.LOGO_FONT.render("PySnake", True, self.LOGO_CURRENT_COLOR)
         self.LOGO_RECT = self.LOGO_TEXT.get_rect()
         self.LOGO_RECT.topleft = (coord)
 
 
-
+        
     def _process_input(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -53,21 +54,18 @@ class Splash_Scene(Scene):
             return True
         if self._shouldQuit:
             quit()
-        if self.LOGO_CURRENT_COLOR == LOGO_COLOR_1:
-            self.LOGO_CURRENT_COLOR = LOGO_COLOR_2
-        elif self.LOGO_CURRENT_COLOR == LOGO_COLOR_2: 
-            self.LOGO_CURRENT_COLOR = LOGO_COLOR_3
-        elif self.LOGO_CURRENT_COLOR == LOGO_COLOR_3:
-            self.LOGO_CURRENT_COLOR = LOGO_COLOR_4
-        elif self.LOGO_CURRENT_COLOR == LOGO_COLOR_4:
-            self.LOGO_CURRENT_COLOR = LOGO_COLOR_5
-        else:
-            self.LOGO_CURRENT_COLOR = LOGO_COLOR_1
+        
+        if self.LOGO_CURRENT_COLOR != len(self.LOGO_COLORS):
+            self.LOGO_CURRENT_COLOR += 1
+        
+        if self.LOGO_CURRENT_COLOR == len(self.LOGO_COLORS):
+            self.LOGO_CURRENT_COLOR = 0
+            self.LOGO_COLORS.reverse()
         return False
 
     def _render_scene(self, window:Surface):
         #pygame.draw.rect(self._scene_surface, self.LOGO_CURRENT_COLOR, self.LOGO_RECT, True)
-        self.LOGO_TEXT = self.LOGO_FONT.render("PySnake", True, self.LOGO_CURRENT_COLOR)
+        self.LOGO_TEXT = self.LOGO_FONT.render("PySnake", True, self.LOGO_COLORS[self.LOGO_CURRENT_COLOR])
         self._scene_surface.blit(self.LOGO_TEXT, self.LOGO_RECT)
         window.blit(self._scene_surface, (self.LOGO_RECT.center))
         pygame.display.flip()
