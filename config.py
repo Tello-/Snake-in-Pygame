e917415d1f178bceb620924fed8aec55302fe3d2
Tello- <lollisjosh@gmail.com>
2021-05-06 21:06:08 -0700

    Severley shrunk config.py

--- config.py
+++ config.py
@@ -3,54 +3,13 @@ from typing import Text
 import pygame
 
 import random
-from enum import Enum
-
-from pygame.mixer import fadeout
-
-import color
 
 random.seed()
 pygame.init()
 
-
-
 GAME_SPEED = 10
 WINDOW_WIDTH = int(600)
 WINDOW_HEIGHT = int(700)
-
-BG_WIDTH = int(WINDOW_WIDTH)
-BG_HEIGHT = BG_WIDTH
-
-CELLS_ACROSS = 21
-CELLS_DOWN = 21
-
-CELL_WIDTH = BG_WIDTH / CELLS_ACROSS
-CELL_HEIGHT = BG_HEIGHT / CELLS_DOWN
-
-GRID_CELL_WIDTH = .90 * CELL_WIDTH
-GRID_CELL_HEIGHT = .90 * CELL_HEIGHT
-
-
-GRID_CELL_OFFSET_X = .05 * CELL_WIDTH
-GRID_CELL_OFFSET_Y = .05 * CELL_HEIGHT
-
-PLAYER_CELL_OFFSET_X = .05 * CELL_WIDTH
-PLAYER_CELL_OFFSET_Y = .05 * CELL_HEIGHT
-
-PLAYER_WIDTH = .85 * CELL_WIDTH 
-PLAYER_HEIGHT = .8 * CELL_HEIGHT 
-
-DEFAULT_HEAD_COORD = [10,10]
-SCORE_FONT = pygame.font.SysFont("", 64)
-
-
-
-def Generate_Random_Coord() -> list[int]:
-    x = random.randint(0, CELLS_ACROSS-1)
-    y = random.randint(0, CELLS_DOWN-1)
-    return [x,y]
-
-
 
 
 # !!!!!!!!!!!!!!!!!!!!!!!!!!
--- scene.py
+++ scene.py
@@ -5,10 +5,10 @@ from typing import Tuple
 from pygame import Surface
 import pygame
 from pygame import time
-from config import GAME_SPEED
-from config import WINDOW_WIDTH,WINDOW_HEIGHT
-from color import LOGO_COLOR_1,LOGO_COLOR_2,LOGO_COLOR_3,LOGO_COLOR_4,LOGO_COLOR_5
+from config import *
+from color import *
 from pygame import rect
+import Debug
 
 
 class Scene:
@@ -72,7 +72,32 @@ class Splash_Scene(Scene):
 class Play_Scene(Scene):
     def __init__(self, size:tuple, coord:tuple = (0,0)):
         super().__init__(size, coord)
-        self._splash_over = False     
+        self._splash_over = False
+
+        self.BG_WIDTH = int(WINDOW_WIDTH)
+        self.BG_HEIGHT = self.BG_WIDTH
+
+        self.self.CELLS_ACROSS = 21
+        self.self.CELLS_DOWN = 21
+
+        self.self.CELL_WIDTH = self.BG_WIDTH / self.self.CELLS_ACROSS
+        self.self.CELL_HEIGHT = self.BG_HEIGHT / self.self.CELLS_DOWN
+
+        self.GRID_self.CELL_WIDTH = .90 * self.self.CELL_WIDTH
+        self.GRID_self.CELL_HEIGHT = .90 * self.self.CELL_HEIGHT
+
+
+        self.self.GRID_CELL_OFFSET_X = .05 * self.self.CELL_WIDTH
+        self.self.GRID_CELL_OFFSET_Y = .05 * self.self.CELL_HEIGHT
+
+        self.PLAYER_CELL_OFFSET_X = .05 * self.self.CELL_WIDTH
+        self.PLAYER_CELL_OFFSET_Y = .05 * self.self.CELL_HEIGHT
+
+        self.PLAYER_WIDTH = .85 * self.self.CELL_WIDTH 
+        self.PLAYER_HEIGHT = .8 * self.self.CELL_HEIGHT 
+
+        self.DEFAULT_HEAD_COORD = [10,10]
+        self.SCORE_FONT = pygame.font.SysFont("", 64)    
 
     def _process_input(self, keys):
         pass
@@ -80,4 +105,95 @@ class Play_Scene(Scene):
         pass
         
     def _render_scene(self, window:Surface):
-        pass
+        self.window.blit(self._GRID_LAYER, (0,0))
+        self._drawSnake()
+        if self.apple != None:
+            if len(self.apple) > 0:
+                self._drawApple()
+            
+        self._initScorePanel(self._SCORE_PANEL_LAYER)
+        
+        window.blit(self._SCORE_PANEL_LAYER, (0, self.BG_HEIGHT))
+        self._score_text = self._SCORE_FONT.render("Points: {}".format(self._points), True, FADED_SCHOOLBUS)
+        window.blit(self._score_text, self._score_text_rect)
+
+        
+        pygame.display.flip()
+
+    def _initFilledBG(self, rgb, surface):
+        rectColor = []
+        if Debug.DEBUG_MODE_ON:
+            rectColor = DB_GREEN
+        else:
+            rectColor = rgb
+
+        #pygame.draw.rect(surface, rectColor, [0, 0, config.BG_WIDTH, config.BG_HEIGHT], 0)
+
+    def _initGridOverlay(self,rgb, surface):
+        rectColor = []
+        if Debug.DEBUG_MODE_ON:
+            rectColor = DB_SNOT
+        else:
+            rectColor = rgb
+
+        for i in range(self.CELLS_ACROSS):
+            for j in range(self.CELLS_DOWN):
+                x = i * self.CELL_WIDTH
+                y = j * self.self.CELL_HEIGHT
+                pygame.draw.rect(surface, rectColor, [x + self.GRID_CELL_OFFSET_X, y + self.GRID_CELL_OFFSET_Y, self.GRID_CELL_WIDTH, self.GRID_self.CELL_HEIGHT], 0)
+
+    def _drawSnake(self):
+        rectColor = []
+        if Debug.DEBUG_MODE_ON:
+            rectColor = color.DB_BLUE
+        else:
+            rectColor = color.SALMON
+
+        snakeCoords = self._snake.snake_coords()
+        for i in range(len(snakeCoords)):
+            pixPos = self._CoordToPixel(snakeCoords[i])            
+            pygame.draw.rect(self._window, rectColor, [pixPos[0], pixPos[1], self.PLAYER_WIDTH, self.PLAYER_HEIGHT], 0)
+
+    def _drawApple(self):
+        rectColor = []
+        if DEBUG_MODE_ON:
+            rectColor = color.DB_ORANGE
+        else:
+            rectColor = color.FADED_SCHOOLBUS
+
+        pixPos = self._CoordToPixel(self.apple)            
+        pygame.draw.rect(self._window, rectColor, [pixPos[0], pixPos[1], self.GRID_self.CELL_WIDTH, self.GRID_self.CELL_HEIGHT], 0)
+
+    def _spawnApple(self) -> list[int]:
+        "Finds an open grid space at random and returns the coordinate"
+        genAgain = True
+        newCoord = None
+        while genAgain:
+            newCoord = self.Generate_Random_Coord()
+            if newCoord in self._snake.snake_coords():
+                genAgain = True
+            else:
+                genAgain = False
+        return newCoord
+
+    def _collectApple(self):
+        "Consumes the current apple so to say and triggers a new spawn and a point increment"
+        self._points += 10
+        self.apple.clear()
+        self.apple = None
+        self._snake._toggleGrow()
+        
+    def _checkAppleCollision(self)->bool:
+        if self.apple != None:
+            if len(self.apple) > 0:
+                for elem in self._snake.snake_coords():
+                    if elem[0] == self.apple[0] and elem[1] == self.apple[1]:
+                        return True
+                
+            return False
+
+    def Generate_Random_Coord(self) -> list[int]:
+        x = random.randint(0, self.CELLS_ACROSS-1)
+        y = random.randint(0, self.CELLS_DOWN-1)
+        return [x,y]
+    
